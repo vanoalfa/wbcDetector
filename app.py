3be4b6a580463909ad77b2e4f6fd30be706e1014
vanoalfa <73095441+vanoalfa@users.noreply.github.com>
2025-04-22 04:59:19 +0000

    Tambah #penjelasan

--- app.py
+++ app.py
@@ -1,57 +1,83 @@
-from flask import Flask
-from threading import Thread
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-from telegram import Update
-from detect import detect_white_blood_cells
-from PIL import Image
-import os
+from flask import Flask #Pustaka untuk server flask
+from threading import Thread #Pustaka untuk menjalankan bot telegram dan server flask secara bersamaan
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext #Pustaka untuk mengambil komponen yang dibutuhkan bot telegram
+#Updater : Untuk menghubungkan bot dengan server Telegram.
+#CommandHandler : Untuk perintah /start atau perintah lain yang dikirimkan oleh pengguna.
+#MessageHandler : Untuk menangani pesan dari pengguna (contoh: gambar).
+#Filters : Untuk menyaring tipe pesan yang diterima (dalam hal ini gambarnya saja).
+#CallbackContext : Objek yang menyimpan data konteks saat callback fungsi dipanggil.
+from telegram import Update #Pustaka untuk update pesan dari telegram
+from detect import detect_white_blood_cells #Mengimpor program deteksi sel darah putih dari file detect.py
+from PIL import Image #Pustaka untuk memproses gambar
+import os #Pustaka untuk mengakses environment variables
 
 #yang ini untuk konfigurasi botnya teman-teman alterians (telegram)
-TOKEN = os.environ.get("API_TELEGRAM")
+TOKEN = os.environ.get("API_TELEGRAM") #ngambil API dari telegram
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Halo! Kirimkan gambar sel darah putih, dan saya akan deteksi.")
-
+#Message awal
+def start(update: Update, context: CallbackContext): #jalan kalau user ketik /start nantinya
+    #update:Update untuk menyimpan data pesan dari user telegram
+    #context:CallbackContext untuk menyimpan data konteks saat callback fungsi dipanggil dalam hal ini untuk mengirim pesan balasan ke user
+    update.message.reply_text("Halo! Kirimkan gambar sel darah putih, dan saya akan beritahu kamu sel apa itu.") #pesan balasan ke user
 
 #udah masuk ke ngurus gambarnya (astagfirullah errornya banyak)
-def handle_image(update: Update, context: CallbackContext):
-    photo = update.message.photo[-1].get_file()
-    file_path = f"static/{photo.file_id}.jpg"
-    photo.download(file_path)
+def handle_image(update: Update, context: CallbackContext): #fungsi untuk menangani gambar yang dikirim user
+    photo = update.message.photo[-1].get_file() #mengambiil file gambar dari pesan user
+    #update.message.photo[-1] : Mengambil gambar terakhir dari pesan yang dikirim user (fungsi dari telegramnya).
+    #.get_file() : Mendapatkan file gambar dari objek gambar tersebut.
+    file_path = f"static/{photo.file_id}.jpg" #menyimpan gambar di folder static dengan nama file_id.jpg (file_id adalah id gambar yang ditentukan dari telegramnya langsung)
+    photo.download(file_path) #download gambar ke folder static
 
-    #Resize gambarnya jadi 640x640 dulu bolo
-    img = Image.open(file_path)
-    img = img.resize((640, 640))
-    img.save(file_path)
-    update.message.reply_text("Gambar sedang diproses...")
+    #Resize gambarnya jadi 640x640 dulu
+    update.message.reply_text("Gambar sedang diproses...") #pesan balasan ke user bahwa gambar sedang diproses
+    img = Image.open(file_path) #membuka gambar yang sudah di download di folder static. dari pustaka PIL
+    img = img.resize((640, 640)) #resize gambar jadi 640x640
+    img.save(file_path) #simpan gambar yang sudah di resize
 
     #ini nanti konek ke detect.py untuk deteksi gambar sell nya
-    result_path, result_message = detect_white_blood_cells(file_path)
+    result_path, result_message = detect_white_blood_cells(file_path) #panggil fungsi detect_white_blood_cells dari file detect.py
 
     #balik lagi kesini dari detect.py, kirim gambar hasil deteksi ke user
-    if result_path:
-        with open(result_path, 'rb') as result_image:
-            update.message.reply_photo(photo=result_image, caption=result_message)
+    if result_path: #ambil gambar hasil deteksi dari detect.py
+        with open(result_path, 'rb') as result_image: #buka gambar hasil deteksi dari folder static untuk dikirim ke user
+        #open(result_path, 'rb'): Membuka file gambar hasil deteksi dari folder static.
+        #result_path adalah path dari gambar hasil deteksi yang sudah disimpan di folder static.
+        #'rb': Mode baca (read) dalam bentuk biner (binary).
+        #as result_image: Menyimpan gambar hasil deteksi ke variabel result_image.
+            update.message.reply_photo(photo=result_image, caption=result_message) #kirim gambar hasil deteksi ke user
+            #photo=result_image: Gambar hasil deteksi yang sudah dibuka.
+            #caption=result_message: Pesan yang akan ditampilkan di bawah gambar hasil deteksi dari detect.py
+        os.remove(result_path) #hapus gambar hasil deteksi dari folder static setelah dikirim ke user
     else:
-        update.message.reply_text(result_message)
+        update.message.reply_text(result_message) #jika tidak ada gambar hasil deteksi, kirim pesan error ke user dan beritahu user bahwa gambar tidak ditemukan
+    #result_path: Path dari gambar hasil deteksi yang sudah disimpan di folder static.
+    #result_image: Gambar dari file di folder static yang sudah di proses pada fungsi detect_white_blood_cells file detect.py.
 
-def start_bot():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
+#disini main dari program botnya dimana program ini yang akan berjalan untuk menjalankan bot telegram
+def start_bot(): #fungsi untuk menjalankan bot telegram
+    updater = Updater(TOKEN, use_context=True) #menghubungkan bot dengan server Telegram
+    dp = updater.dispatcher #membuat objek dispatcher untuk menangani pesan dari user
+    #dp adalah singkatan dari dispatcher yang merupakan objek yang digunakan untuk mengatur dan mengelola handler (fungsi) yang akan dipanggil saat pesan tertentu diterima.
+    #updater.dispatcher : Mendapatkan objek dispatcher dari updater.
+    #Dispatcher adalah objek yang mengatur dan mengelola handler (fungsi) yang akan dipanggil saat pesan tertentu diterima.
+    dp.add_handler(CommandHandler("start", start)) #menambahkan handler untuk perintah /start
+    dp.add_handler(MessageHandler(Filters.photo, handle_image)) #Jika user kirim gambar → jalankan handle_image().
+    #filters.photo : Menyaring pesan yang hanya berisi gambar.
+    #handle_image : Fungsi yang akan dipanggil saat pesan gambar diterima.
+    updater.start_polling() #mulai bot telegram
+    updater.idle() #membuat bot tetap berjalan sampai diinterupsi secara manual
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.photo, handle_image))
-
-    updater.start_polling()
-    updater.idle()
-
-#Pokoknya ini buat nampilin server flasknya berjalan
+#Pokoknya ini buat server flasknya
 app = Flask(__name__)
+#app adalah variabel yang akan digunakan untuk mengakses fungsi-fungsi dari Flask.
+#Flask(): adalah fungsi pada pustaka Flask yang digunakan untuk membuat objek aplikasi Flask.
+#__name__ adalah variabel khusus di Python yang berisi nama modul saat ini
 
-@app.route('/')
+@app.route('/') #@app.route('/') adalah decorator yang digunakan untuk menentukan URL yang akan dipetakan ke fungsi home(). refrensinya: https://flask.palletsprojects.com/en/2.0.x/quickstart/#routing
+#kenapa '/'? karena ini adalah URL root dari server flask, yaitu URL yang akan dipetakan ke fungsi home() saat server flask dijalankan. misal kalau mau ditambah seperti: '/project' maka nanti urlnya akan membawa ke url halaman projectnya (urlnya akan menjadi https://<nama domain>/project).
 def home():
-    return "Server WBC detection sedang berjalan, Telegram @WBCDetectorBot dapat digunakan."
+    return "Server WBC detection sedang berjalan, Telegram @WBCDetectorBot dapat digunakan." #tampilan pada browser ketika server flask dijalankan
 
-if __name__ == '__main__':
-    Thread(target=start_bot).start()
-    app.run(host='0.0.0.0', port=5000) 
+if __name__ == '__main__': #ini buat menjalankan bot telegram dan server flask secara bersamaan karena __name__ == '__main__' berarti program ini sedang dijalankan sebagai program utama, bukan sebagai modul yang diimpor. diambil dari https://www.geeksforgeeks.org/__name__-special-variable-python/
+    Thread(target=start_bot).start() #menjalankan bot telegram
+    app.run(host='0.0.0.0', port=5000) #menjalankan server flask di port 5000 dan host 0.0.0.0 agar bisa diakses dari luar (dari internet).
--- detect.py
+++ detect.py
@@ -3,87 +3,109 @@ from roboflow import Roboflow
 from PIL import Image, ImageDraw, ImageFont
 
 #Inisialisasi Roboflow
-api_key = os.environ.get("API_ROBOFLOW")
-if not api_key:
-    raise ValueError("API_ROBOFLOW belum di-set di environment variables!")
+api_key = os.environ.get("API_ROBOFLOW") #Ambil API ROBOFLOW key dari environment variables
+if not api_key: #Jika API_ROBOFLOW belum di-set di environment variables, maka akan muncul error
+    raise ValueError("API_ROBOFLOW belum di-set di environment variables!") #Error yang akan muncul jika API_ROBOFLOW belum di-set di environment variables
 
-rf = Roboflow(api_key=api_key)
-project = rf.workspace().project("blood-cell-classification-juuw1")
-model = project.version("2").model
+rf = Roboflow(api_key=api_key) #Inisialisasi Roboflow dengan API key yang sudah di-set di environment variables
+#rf: variabel yang akan digunakan untuk mengakses fungsi-fungsi dari Roboflow.
+project = rf.workspace().project("blood-cell-classification-juuw1") #Ambil project dari Roboflow workspace
+model = project.version("2").model #versi model yang digunakan
 
 #Fungsi untuk mendeteksi sel darah putih, fungsi ini akan dipanggil dari app.py
 def detect_white_blood_cells(image_path):
-    try:
+    try: #menggunakan try-except untuk menangani error yang mungkin terjadi saat mendeteksi gambar
         #Prediksi gambar
-        prediction = model.predict(image_path).json()
-        detections = prediction['predictions']
+        prediction = model.predict(image_path).json() #Mengirim gambar ke Roboflow untuk diprediksi
+        #.json() ditambahkan karena hasil prediksi dari Roboflow adalah dalam bentuk json, jadi harus diubah dulu ke json baru kemudian bisa diproses oleh python.
+        detections = prediction['predictions'] #Ambil hasil prediksi dari json yang sudah diubah ke python
 
-        if len(detections) == 0:
-            return None, "Tidak ditemukan sel darah putih pada gambar."
+        if len(detections) == 0: #Jika tidak ada sel darah putih yang terdeteksi
+            return None, "Tidak ditemukan sel darah putih pada gambar." #maka akan mengirimkan pesan ke user bahwa tidak ada sel darah putih yang terdeteksi
 
         #Buka gambar asli dari pathnya
-        image = Image.open(image_path)
-        draw = ImageDraw.Draw(image)
+        image = Image.open(image_path) #Membuka gambar yang sudah di download di folder static. dari pustaka PIL
+        draw = ImageDraw.Draw(image) #Membuat variabel ImageDraw untuk memberikan visualisasi pada gambar
 
         #font buat tulis teks di bounding box
         try:
-            font = ImageFont.truetype("arialbd.ttf", size=22)
+            font = ImageFont.truetype("arialbd.ttf", size=22) #Coba gunakan font Arial Bold
         except:
-            font = ImageFont.load_default()
+            font = ImageFont.load_default() #Jika font Arial Bold tidak ditemukan, gunakan font default yang ada di PIL
 
         #Hitung jumlah class & total confidence
-        class_counts = {}
-        total_confidence = 0
+        class_counts = {} #Dictionary untuk menghitung jumlah class
+        total_confidence = 0 #variabel untuk menghitung total confidence, = 0 karena pada awalnya belum ada confidence yang dihitung.
 
-        for det in detections:
-            class_name = det['class']
-            confidence = det['confidence'] * 100  #Dalam bentuk persen
-            total_confidence += confidence
+        for det in detections: #Looping untuk setiap deteksi sel yang ada di gambar
+            class_name = det['class'] #ambil nama class dari roboflow
+            #det['class']: Nama class dari deteksi sel yang ada di gambar, diambil dari json yang sudah diubah ke python.]
+            confidence = det['confidence'] * 100  #Ubah confidence dari desimal ke persen
+            total_confidence += confidence #Tambahkan confidence ke total confidence
 
             #Hitung jumlah class
-            class_counts[class_name] = class_counts.get(class_name, 0) + 1
+            class_counts[class_name] = class_counts.get(class_name, 0) + 1 #Tambahkan jumlah class ke dictionary class_counts
 
             #Ambil posisi bounding box
-            x, y, w, h = det['x'], det['y'], det['width'], det['height']
+            x, y, w, h = det['x'], det['y'], det['width'], det['height'] #Ambil posisi bounding box dari roboflow
 
             #Konversi ke koordinat box
-            left = x - w / 2
-            top = y - h / 2
-            right = x + w / 2
-            bottom = y + h / 2
+            left = x - w / 2 #Koordinat kiri bounding box
+            top = y - h / 2 #Koordinat atas bounding box
+            right = x + w / 2 #Koordinat kanan bounding box
+            bottom = y + h / 2 #Koordinat bawah bounding box
 
             #Gambar bounding box
-            draw.rectangle([left, top, right, bottom], outline="blue", width=2)
+            draw.rectangle([left, top, right, bottom], outline="blue", width=2) #Gambar bounding box di gambar yang sudah di download di folder static dan sudah di deteksi
 
             #Tulis label
-            label = f"{class_name} ({confidence:.1f}%)"
-            draw.text((left, top - 10), label, fill="blue", font=font)
-
+            label = f"{class_name} ({confidence:.1f}%)" #Label yang akan ditulis di bounding box
+            #class_name: Nama class dari deteksi sel yang ada di gambar, diambil dari json yang sudah diubah ke python.
+            #confidence:.1f: Confidence dari deteksi sel yang ada di gambar, diambil dari json yang sudah diubah ke python.
+            #.1f: Format angka desimal dengan 1 angka di belakang koma.
+            draw.text((left, top - 10), label, fill="blue", font=font) #Tulis label di bounding box
+            #(left, top - 10): Koordinat teks yang akan ditulis di bounding box.
+            #-10: Jarak antara teks dan bounding box.
+            #label: Label yang akan ditulis di bounding box dan sudah di set sebelumnya.
+            #fill="blue": Warna teks.
+            #font=font: Font yang akan digunakan untuk menulis teks.
+            #font: Font yang sudah di-set di atas.
+            
             #Gambar latar rectangle di belakang teks
-            text_bbox = draw.textbbox((0,0), label, font=font)
-            text_width = text_bbox[2] - text_bbox[0]
+            text_bbox = draw.textbbox((0,0), label, font=font) #Ambil bounding box dari teks
+            #draw.textbbox((0,0), label, font=font): Mengambil bounding box dari teks yang akan ditulis di bounding box.
+            #draw: Variabel ImageDraw yang sudah di-set sebelumnya pada line 28
+            #textbbox: pustaka PIL untuk mengambil bounding box dari teks.
+            text_width = text_bbox[2] - text_bbox[0] #Lebar teks
+            #text_bbox[2]: angka 2 adalah koordinat kanan dari bounding box teks.
+            #text_bbox[0]: angka 0 adalah koordinat kiri dari bounding box teks.
             text_height = text_bbox[3] - text_bbox[1]
+            #text_bbox[3]: angka 3 adalah koordinat bawah dari bounding box teks.
+            #text_bbox[1]: angka 1 adalah koordinat atas dari bounding box teks.
+            #refrensi: https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html#PIL.ImageDraw.ImageDraw.textbbox    
+            background_rect = [left, top - text_height - 4, left + text_width + 6, top] #variabel kotak untuk latar teks
+            draw.rectangle(background_rect, fill="blue") #Gambar kotak untuk latar teks
+            draw.text((left + 3, top - text_height - 1), label, fill="white", font=font) #tulis teks di atas kotak background_rect
             
-            background_rect = [left, top - text_height - 4, left + text_width + 6, top]
-            draw.rectangle(background_rect, fill="blue")
-
-            #Tulis teks lagi di atas kotak
-            draw.text((left + 3, top - text_height - 1), label, fill="white", font=font)
-            
-        avg_confidence = total_confidence / len(detections)
-
+        avg_confidence = total_confidence / len(detections) #Hitung rata-rata confidence
+        #total_confidence: Total confidence dari semua deteksi sel yang ada di gambar.
+        #len(detections): Jumlah deteksi sel yang ada di gambar.
+        
         #Simpan gambar hasil deteksi
-        result_path = image_path.replace(".jpg", "_detected.jpg")
-        image.save(result_path)
+        result_path = image_path.replace(".jpg", "_detected.jpg") #Ganti nama file gambar hasil deteksi dengan menambahkan "_detected" di belakang nama file asli. sebagai contoh: 123.jpg menjadi 123_detected.jpg
+        #.replace merupakan fungsi dari python untuk mengganti string dengan string lain. refrensinya: https://www.w3schools.com/python/ref_string_replace.asp
+        image.save(result_path) #Simpan gambar hasil deteksi di folder static
 
         #Susun pesan hasil deteksi
-        result_message = "Sel terdeteksi:\n"
-        for class_name, count in class_counts.items():
-            result_message += f"- {class_name}: {count} sel\n"
+        result_message = "Sel terdeteksi:\n" #Pesan yang akan dikirim ke user
+        for class_name, count in class_counts.items(): #Looping untuk setiap class yang ada di dictionary class_counts
+            result_message += f"> {class_name}: {count} sel\n" #Tambahkan jumlah class ke pesan yang akan dikirim ke user. jadi nanti muncul diusernya seperti: - Eosinophil: 1 sel, > Lymphocyte: 2 sel
 
-        result_message += f"\nNilai confidence rata-rata: {avg_confidence:.2f}%"
+        result_message += f"\nNilai confidence rata-rata: {avg_confidence:.2f}%" #Tambahkan rata-rata confidence ke pesan yang akan dikirim ke user. nanti munculnya seperti: Nilai confidence rata-rata: 95.00%
 
-        return result_path, result_message
+        return result_path, result_message #Kembalikan path gambar hasil deteksi dan pesan hasil deteksi ke app.py
+        #result_path: Path dari gambar hasil deteksi yang sudah disimpan di folder static.
+        #result_message: Pesan yang akan dikirim ke user.
 
     except Exception as e:
-        return None, f"Terjadi kesalahan saat mendeteksi: {e}"
+        return None, f"Terjadi kesalahan saat mendeteksi: {e}" #Jika terjadi error saat mendeteksi gambar, maka akan mengirimkan pesan error ke user dan beritahu user bahwa terjadi kesalahan saat mendeteksi gambar. misalnya: Terjadi kesalahan saat mendeteksi: [Errno 2] No such file or directory: 'static/123.jpg'
